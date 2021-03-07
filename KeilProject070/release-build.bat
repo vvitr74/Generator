627dd91f0a79660952707c19bd1bed493ffd7115
Danil Ruban <intx82@gmail.com>
2021-03-07 21:34:36 +0200

    Фикс версии

--- KeilProject070/release-build.bat
+++ KeilProject070/release-build.bat
@@ -4,7 +4,7 @@ c:\Keil_v5\UV4\uv4 -b Generator070_release.uvprojx -t "Target 1" -o "release-bui
 
 bootloader-tools\hex2bin.exe Objects\gen-g070-release.hex
 
-bootloader-tools\ba-fw-builder.exe -v "0.2.1" -ib Objects\gen-g070-release.bin -t dev_gen_g070 -o dev_gen_g070
+bootloader-tools\ba-fw-builder.exe -v "0.2.3" -ib Objects\gen-g070-release.bin -t dev_gen_g070 -o dev_gen_g070
 
 bootloader-tools\srec_cat.exe ba.hex -Intel romba.hex -Intel Objects\gen-g070-release.bin -Binary -offset 0x08003800 -o dev_gen_g070_release.hex -Intel
 
